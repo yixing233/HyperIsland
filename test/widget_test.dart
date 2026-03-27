13236a9240015aa492fce01255b01aa3e06f86d0
yusufyorunc <yusufyorunc65@gmail.com>
2026-03-27 15:38:25 +0300

    fix(test): correct package name casing in widget test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hyperisland/main.dart';
+import 'package:hyperIsland/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
